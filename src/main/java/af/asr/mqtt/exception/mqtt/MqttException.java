eaa8f7fb57121ab5051147fe00c05809a71d6ec8
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-25 13:52:59 +0430

    resource management

--- src/main/java/af/asr/mqtt/exception/mqtt/MqttException.java
+++ src/main/java/af/asr/mqtt/exception/mqtt/MqttException.java
@@ -1,10 +1,11 @@
 package af.asr.mqtt.exception.mqtt;
 
+import af.asr.mqtt.exception.common.BaseCheckedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-public class MqttException extends RuntimeException{
+public class MqttException extends BaseCheckedException {
     public MqttException(String message) {
         super(message);
     }
---
+++ src/main/java/af/asr/mqtt/resource/MqttApiResource.java
@@ -0,0 +1,58 @@
+package af.asr.mqtt.resource;
+
+
+import af.asr.mqtt.config.Mqtt;
+import af.asr.mqtt.data.MqttPublishModel;
+import af.asr.mqtt.data.MqttSubscribeModel;
+import af.asr.mqtt.exception.mqtt.ExceptionMessages;
+import af.asr.mqtt.exception.mqtt.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping(value = "/api/mqtt")
+public class MqttApiResource {
+
+    @PostMapping("publish")
+    public void publishMessage(@RequestBody @Valid MqttPublishModel messagePublishModel,
+                               BindingResult bindingResult) throws org.eclipse.paho.client.mqttv3.MqttException, MqttException {
+        if (bindingResult.hasErrors()) {
+            throw new MqttException(ExceptionMessages.SOME_PARAMETERS_INVALID);
+        }
+
+        MqttMessage mqttMessage = new MqttMessage(messagePublishModel.getMessage().getBytes());
+        mqttMessage.setQos(messagePublishModel.getQos());
+        mqttMessage.setRetained(messagePublishModel.getRetained());
+
+        Mqtt.getInstance().publish(messagePublishModel.getTopic(), mqttMessage);
+    }
+
+    @GetMapping("subscribe")
+    public List<MqttSubscribeModel> subscribeChannel(@RequestParam(value = "topic") String topic,
+                                                     @RequestParam(value = "wait_millis") Integer waitMillis)
+            throws InterruptedException, org.eclipse.paho.client.mqttv3.MqttException {
+        List<MqttSubscribeModel> messages = new ArrayList<>();
+        CountDownLatch countDownLatch = new CountDownLatch(10);
+        Mqtt.getInstance().subscribeWithResponse(topic, (s, mqttMessage) -> {
+            MqttSubscribeModel mqttSubscribeModel = new MqttSubscribeModel();
+            mqttSubscribeModel.setId(mqttMessage.getId());
+            mqttSubscribeModel.setMessage(new String(mqttMessage.getPayload()));
+            mqttSubscribeModel.setQos(mqttMessage.getQos());
+            messages.add(mqttSubscribeModel);
+            countDownLatch.countDown();
+        });
+
+        countDownLatch.await(waitMillis, TimeUnit.MILLISECONDS);
+
+        return messages;
+    }
+
+
+}
