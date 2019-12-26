f1fad8be7161884ef648352b49c8e6b6531da855
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-26 11:31:41 +0430

    refactoring

--- src/main/java/af/asr/mqtt/config/Mqtt.java
+++ src/main/java/af/asr/mqtt/config/Mqtt.java
@@ -1,22 +1,35 @@
 package af.asr.mqtt.config;
 
-import af.asr.mqtt.util.MqttServerConstants;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Component
 public class Mqtt {
 
     private static IMqttClient instance;
 
+    public  static String MQTT_PUBLISHER_ID;
+    public  static String MQTT_SERVER_ADDRESS;
+
+    @PostConstruct
+    public void init()
+    {
+        System.out.println("PublisherId: "+ MQTT_PUBLISHER_ID);
+    }
+
     public static IMqttClient getInstance() {
         try {
             if (instance == null) {
-                instance = new MqttClient(MqttServerConstants.MQTT_PUBLISHER_ID, MqttServerConstants.MQTT_SERVER_ADDRESS);
+                instance = new MqttClient(MQTT_PUBLISHER_ID, MQTT_SERVER_ADDRESS);
             }
 
             MqttConnectOptions options = new MqttConnectOptions();
@@ -32,6 +45,19 @@ public class Mqtt {
         }
 
         return instance;
+    }
+
+    @Value("${mqtt.publisher_id}")
+    public void setMqttPublisherId(String publisherId)
+    {
+        this.MQTT_PUBLISHER_ID = publisherId;
+    }
+
+
+    @Value("${mqtt.server_address}")
+    public void setMqttServerAddress(String serverAddress)
+    {
+        this.MQTT_SERVER_ADDRESS = serverAddress;
     }
 
 }
--- src/main/java/af/asr/mqtt/util/MqttServerConstants.java
+++
@@ -1,7 +0,0 @@
-package af.asr.mqtt.util;
-
-public class MqttServerConstants {
-
-    public final static String MQTT_PUBLISHER_ID ="asr-mqtt-server";
-    public final static String MQTT_SERVER_ADDRESS= "tcp://127.0.0.1:1883";
-}
